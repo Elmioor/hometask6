87cab86439c0110e0d8ae7844a595c8b25e57655
Eljees <yurytumanov.r@yandex.ru>
2020-11-27 18:29:37 +0300

    4rd task

--- main.py
+++ main.py
@@ -74,6 +74,62 @@
 
 
 # 4.Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+#
+# class Car:
+#     speed: int
+#     color: str
+#     name: str
+#     is_police: bool
+#
+#     def method_go(self):
+#         return "the vehicle has stared moving"
+#         pass
+#
+#     def method_stop(self):
+#         return 'the vehicle has stopped'
+#         pass
+#
+#     def method_turn(self, orientation = 'straight'):
+#         return f"the vehicle has turned {orientation}"
+#         pass
+#
+#     def show_speed(self, speed = 666):
+#         return speed
+#
+# class TownCar(Car):
+#     def show_speed(self, speed = 666):
+#         if speed > 60:
+#             print("You are busted")
+#
+#     pass
+#
+#
+# class SportCar(Car):
+#     pass
+#
+# class WorkCar(Car):
+#     def show_speed(self, speed = 666):
+#         if speed > 40:
+#             print("You are busted")
+#         else:
+#             print("drive safely")
+#
+#
+# class PoliceCar(Car):
+#     pass
+#
+#
+#
+# carobject = Car()
+# #print(carobject.show_speed(),carobject.method_turn())
+# wor_c = WorkCar()
+#
+# #print(wor_c.show_speed(666))
+#
+# town_o = TownCar()
+#
+# print(town_o.show_speed())
+
 
 # 5. Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
 
